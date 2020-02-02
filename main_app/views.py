ea3e917b7a13f7f191861f9f4f4b405e9db8d0e4
Michael Martinez <rusalka2100@gmail.com>
2020-02-02 14:46:23 -0600

    views.py finished

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Score
+from django.views.generic.edit import CreateView, DeleteView
 
-# Create your views here.
+def index(request):
+    scores = Score.objects.all()
+    return render(request, 'index.html', {'scores': scores})
+
+class ScoreCreate(CreateView):
+    model = Score
+    fields = '__all__'
+    success_url = '/'
+
+class ScoreDelete(DeleteView):
+    model = Score
+    success_url = '/'
