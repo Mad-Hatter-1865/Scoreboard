cb430cb1be93b0605c42d35a29c3fa7359c669f3
Michael Martinez <rusalka2100@gmail.com>
2020-02-02 14:57:52 -0600

    Final urlpatterns added

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('add/',views.ScoreCreate.as_view(), name='scores_create'),
+    path('<int:pk>/delete/', views.ScoreDelete.as_view(), name='scores_delete'),
 ]
